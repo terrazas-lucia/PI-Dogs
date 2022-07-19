98ec678b98dff28e913dfe5a36e39757fc81d008
terrazas-lucia <luciabelenterrazas@gmail.com>
2022-07-19 02:00:48 -0300

    ashuda

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Switch>
         <Route exact path= '/' component={ LandingPage }/> 
         <Route exact path = '/home' component= { Home }/>
-        <Route exact path = '/dogs' component= { CreateDog }/>
+{/*         <Route exact path = '/dogs' component= { CreateDog }/> */}
         <Route exact path = '/dogs/:id' component= { Details }/>
       </Switch>
     </div>
